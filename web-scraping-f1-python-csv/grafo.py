6a62602aaf8a05bbe41d7ba25d20236f34f92501
Gustavo-Rodrigues1 <gustavo3807@academico.ufs.br>
2025-07-26 13:17:28 -0300

    grafo.py teams logic

--- web-scraping-f1-python-csv/grafo.py
+++ web-scraping-f1-python-csv/grafo.py
@@ -1,41 +1,44 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import numpy as np
+import os
 #Leitura das informações dos arquivos CSV para um dataframe
-df_racers = pd.read_csv("webScraping-F1-Python-CSV\\data\\pontuacoes_pilotos.csv")
-# df_teams = pd.read_csv("webScraping-F1-Python-CSV\\pontuacoes_equipes.csv")
+base_path = "web-scraping-f1-python-csv"
+df_racers = pd.read_csv(os.path.join(base_path, "data", "pontuacoes_pilotos.csv"))
+df_teams = pd.read_csv(os.path.join(base_path, "data", "pontuacoes_equipes.csv"))
 
 #Garante que Round esteja em inteiro
 df_racers['round'] = df_racers['round'].astype(int)
 
 # Cores baseadas nos uniformes da temporada atual
 team_color = {
-    "Red Bull Racing": "#1E41FF",       # azul escuro
+    "Red Bull": "#1E41FF",       # azul escuro
     "Ferrari": "#DC0000",        # vermelho
     "Mercedes": "#00D2BE",       # azul-piscina
     "McLaren": "#FF8700",        # laranja
     "Aston Martin": "#006F62",   # verde escuro
-    "Alpine": "#0090FF",         # azul
+    "Alpine F1 Team": "#0090FF",         # azul
     "Williams": "#005AFF",       # azul royal
-    "RB": "#6692FF",             # azul claro
-    "Kick Sauber": "#52E252",    # verde
-    "Haas F1 Team ": "#B6BABD",           # cinza claro
+    "RB F1 Team": "#6692FF",             # azul claro
+    "Sauber": "#52E252",    # verde
+    "Haas F1 Team": "#B6BABD"   # cinza claro
 }
 
 racer_team = {
     #Alpine
-    "Jack Doohan": "Alpine",
-    "Pierre Gasly": "Alpine",
-    "Franco Colapinto": "Alpine",
+    "Jack Doohan": "Alpine F1 Team",
+    "Pierre Gasly": "Alpine F1 Team",
+    "Franco Colapinto": "Alpine F1 Team",
     #Red bull racing
-    "Max Verstappen": "Red Bull Racing",
-    "Yuki Tsunoda": "Red Bull Racing",
+    "Max Verstappen": "Red Bull",
+    "Yuki Tsunoda": "Red Bull",
     #Kick Sauber
-    "Gabriel Bortoleto": "Kick Sauber",
-    "Nico Hülkenberg": "Kick Sauber",
+    "Gabriel Bortoleto": "Sauber",
+    "Nico Hülkenberg": "Sauber",
     #Rb
-    "Isack Hadjar": "RB",
-    "Liam Lawson": "RB",
+    "Isack Hadjar": "RB F1 Team",
+    "Liam Lawson": "RB F1 Team",
     #Ferrari
     "Lewis Hamilton": "Ferrari",
     "Charles Leclerc": "Ferrari",
@@ -53,23 +56,33 @@ racer_team = {
     "Oscar Piastri": "McLaren",
     #Haas F1 Team
     "Esteban Ocon": "Haas F1 Team",
-    "Oliver Bearman": "Haas F1 Team",
-    
+    "Oliver Bearman": "Haas F1 Team"
 }
 
-# Total de pontos finais por piloto
-total_points = df_racers.groupby("nome")["pontos"].max().sort_values(ascending=False)
-top_10 = total_points.head(10).index
-bottom_10 = total_points.tail(10).index
+# Total de pontos finais por piloto divido em duas partes
+total_points_racers = df_racers.groupby("nome")["pontos"].max().sort_values(ascending=False)
+top_10 = total_points_racers.head(10).index
+bottom_10 = total_points_racers.tail(10).index
 
 df_top10 = df_racers[df_racers["nome"].isin(top_10)]
 df_bottom10 = df_racers[df_racers["nome"].isin(bottom_10)]
 
+# Total de pontos finais por equipes
+total_points_team = df_teams.groupby("nome")["pontos"].max().sort_values(ascending=False)
 # Criação dos subplots
-fig, axs = plt.subplots(1, 2, figsize=(20, 8), sharex=True)
+
+fig = plt.figure(figsize=(20, 10))
+gs = gridspec.GridSpec(2, 2, height_ratios=[1, 1])
+
+# Parte de cima: dois gráficos lado a lado
+ax1 = fig.add_subplot(gs[0, 0])
+ax2 = fig.add_subplot(gs[0, 1])
+
+# Parte de baixo: um gráfico ocupando a largura total
+ax3 = fig.add_subplot(gs[1, :])  # <- ocupa todas as colunas
 
 # Função para plotar em um eixo específico
-def plot_subplot(ax, df, title):
+def plot_subplot_racer(ax, df, title):
     for racer_name, racer_data in df.groupby("nome"):
         sorted_data = racer_data.sort_values("round")
         team = racer_team.get(racer_name, "Outro")
@@ -81,17 +94,34 @@ def plot_subplot(ax, df, title):
         abreviacao = last_row["abreviacao"]
         ax.text(last_row["round"] + 0.1, last_row["pontos"], abreviacao,
                 fontsize=9, fontweight='bold', color=color, va='center')
-    ax.set_title(title, fontsize=12)
-    ax.set_ylabel("Pontos acumulados", fontsize=12)
+    ax.set_title(title, fontsize=14, fontweight="bold")
+    ax.set_xlabel("GP", fontsize=12, fontweight="bold")
+    ax.set_ylabel("Pontos acumulados", fontsize=12, fontweight="bold")
+    ax.grid(True)
+    ax.legend(fontsize=8)
+
+def plot_subplot_team(ax, df, title):
+    for team_name, team_data in df.groupby("nome"):
+        sorted_data = team_data.sort_values("round")
+        color = team_color.get(team_name, "gray")
+        ax.plot(sorted_data["round"], sorted_data["pontos"], marker='o', label=team_name, color=color)
+        for x, y in zip(sorted_data["round"], sorted_data["pontos"]):
+            ax.text(x, y + 2, str(y), fontsize=7, ha='center', va='bottom', color=color)
+    ax.set_title(title, fontsize=14, fontweight="bold")
+    ax.set_xlabel("GP", fontsize=12, fontweight="bold")
+    ax.set_ylabel("Pontos acumulados", fontsize=12, fontweight="bold")
     ax.grid(True)
     ax.legend(fontsize=8)
 
 # Gráficos
-plot_subplot(axs[0], df_top10, "Top 10 Primeiros Pilotos - Pontuação Acumulada")
-plot_subplot(axs[1], df_bottom10, "10 Últimos Pilotos - Pontuação Acumulada")
-axs[0].set_xlabel("GP", fontsize=12)
-axs[1].set_xlabel("GP", fontsize=12)
-axs[0].set_xticks(np.arange(df_racers["round"].min(), df_racers["round"].max() + 1))
-axs[1].set_xticks(np.arange(df_racers["round"].min(), df_racers["round"].max() + 1))
+plot_subplot_racer(ax1, df_top10, "Top 10 Primeiros Pilotos - Pontuação Acumulada")
+plot_subplot_racer(ax2, df_bottom10, "10 Últimos Pilotos - Pontuação Acumulada")
+plot_subplot_team(ax3, df_teams, "Pontuação do Campeonato de Construtores de 2025")
+
+
+# Ajuste dos eixos x
+for ax in [ax1, ax2, ax3]:
+    ax.set_xticks(np.arange(df_racers["round"].min(), df_racers["round"].max() + 1))
+
 plt.tight_layout()
-plt.savefig("webScraping-F1-Python-CSV\\data\\pontuacoes_pilotos.png", dpi=300, bbox_inches='tight')
+plt.savefig("web-scraping-f1-python-csv\\data\\pontuacoes_pilotos.png", dpi=300, bbox_inches='tight')
